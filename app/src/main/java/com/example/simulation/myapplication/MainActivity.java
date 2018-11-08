36ae8d0e71ab0a829e19d555847b7330552c4850
DvirDadon <dd2640@bs.amalnet.k12.il>
2018-11-08 13:58:15 -0800

    Mission_num1 end.

---
+++ app/src/main/java/com/example/simulation/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.simulation.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn);
+    }
+
+
+    public void btnclick(View view) {
+
+        btn.setText("Oh Yea, I've been clicked");
+    }
+}
